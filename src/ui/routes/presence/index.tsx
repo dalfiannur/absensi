47a1e429f55e87dfbbce748293da448666857759
unknown <deapratiwiputri490@gmail.com>
2019-10-28 16:24:38 +0700

    revisi terbaru

--- src/ui/routes/presence/index.tsx
+++ src/ui/routes/presence/index.tsx
@@ -11,6 +11,7 @@ import { AppState } from 'store';
 import { Dispatch } from 'redux';
 import { setPresenceTypes } from 'store/presence-type/actions';
 import { connect } from 'react-redux';
+import Particles from 'react-particles-js';
 
 const useStyles = makeStyles(theme => ({
   TextField: {
@@ -46,7 +47,7 @@ const useStyles = makeStyles(theme => ({
     textAlign: 'center',
     left: '25%',
     right: '25%',
-    fontSize: 20
+    fontSize: 25
   },
   ButtonLogin: {
     position: 'absolute',
@@ -69,6 +70,109 @@ interface PresenceProps {
   PresenceType: PresenceTypeState,
   setPresenceTypes: (types: PresenceType[]) => void
 }
+
+
+const particlesJSON = {
+  "particles": {
+          "number": {
+              "value": 50,
+              "density": {
+                  "enable": true,
+                  "value_area": 700 //Denser the smaller the number.
+              }
+          },
+          "color": { //The color for every node, not the connecting lines.
+              "value": "#01579b" //Or use an array of colors like ["#9b0000", "#001378", "#0b521f"]
+          },
+          "shape": {
+              "type": "circle", // Can show circle, edge (a square), triangle, polygon, star, img, or an array of multiple.
+              "stroke": { //The border
+                  "width": 1,
+                  "color": "#145ea8"
+              },
+              "polygon": { //if the shape is a polygon
+                  "nb_sides": 5
+              },
+              "image": { //If the shape is an image
+                  "src": "",
+                  "width": 100,
+                  "height": 100
+              }
+          },
+          "opacity": {
+              "value": 0.7,
+              "random": true
+          },
+          "size": {
+              "value": 10,
+              "random": true
+          },
+          "line_linked": {
+              "enable": true,
+              "distance": 200, //The radius before a line is added, the lower the number the more lines.
+              "color": "#007ecc",
+              "opacity": 0.5,
+              "width": 2
+          },
+          "move": {
+              "enable": true,
+              "speed": 2,
+              "direction": "top", //Move them off the canvas, either "none", "top", "right", "bottom", "left", "top-right", "bottom-right" et cetera...
+              "random": true,
+              "straight": false, //Whether they'll shift left and right while moving.
+              "out_mode": "out", //What it'll do when it reaches the end of the canvas, either "out" or "bounce".
+              "bounce": false, 
+              "attract": { //Make them start to clump together while moving.
+                  "enable": true,
+                  "rotateX": 600,
+                  "rotateY": 1200
+              }
+          }
+      },
+    //Negate the default interactivity
+    "interactivity": {
+          "detect_on": "canvas",
+          "events": {
+              "onhover": {
+                  "enable": false,
+                  "mode": "repulse"
+              },
+              "onclick": {
+                  "enable": false,
+                  "mode": "push"
+              },
+              "resize": true
+          },
+          "modes": {
+              "grab": {
+                  "distance": 800,
+                  "line_linked": {
+                      "opacity": 1
+                  }
+              },
+              "bubble": {
+                  "distance": 800,
+                  "size": 80,
+                  "duration": 2,
+                  "opacity": 0.8,
+                  "speed": 3
+              },
+              "repulse": {
+                  "distance": 400,
+                  "duration": 0.4
+              },
+              "push": {
+                  "particles_nb": 4
+              },
+              "remove": {
+                  "particles_nb": 2
+              }
+          }
+      },
+      "retina_detect": true
+  }
+  
+  // particlesJS("particles-js", particlesJSON)
 
 const PresenceRoute = (props: PresenceProps) => {
   const classes = useStyles();
@@ -106,6 +210,8 @@ const PresenceRoute = (props: PresenceProps) => {
     })
   }
 
+   
+
   const handleTextField = async (e: any) => {
     if (e.keyCode === 13) {
       fetch(`${process.env.REACT_APP_API}/user/${e.target.value}/nik`)
@@ -141,8 +247,28 @@ const PresenceRoute = (props: PresenceProps) => {
   }
 
   return (
+    
     <React.Fragment>
       <div>
+      <Particles
+        params={{
+          particles: {
+            line_linked: {
+              shadow: {
+                enable: true,
+                color: '#FF0000',
+                blur: 2
+              }
+            }
+          }
+        }}
+        style={{
+          position: 'absolute',
+          top: 0,
+          bottom: 0,
+          left: 0,
+          right: 0
+        }} />
         <button
           className={classes.ButtonLogin}
           onClick={() => history.push('/login')}>Login</button>
