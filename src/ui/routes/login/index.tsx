070c6ae03695bad35425ead0bfa5994017ac3d8c
typeofname <dalfiannur@gmail.com>
2019-10-31 15:46:10 +0700

    Move Styles

--- src/ui/routes/login/index.tsx
+++ src/ui/routes/login/index.tsx
@@ -1,81 +1,15 @@
 import * as React from 'react'
-import { makeStyles } from '@material-ui/core/styles'
 import { Grid, TextField } from '@material-ui/core'
 import history from 'utils/history'
 import Logo from '../../../logo.png'
 import BackgroundImage from '../../../assets/background-login.jpg'
 import BackgroundImage2 from '../../../assets/background-login-2.jpg'
-
+import { useStyle } from './style'
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-
-const useStyle = makeStyles(theme => ({
-  Container: {
-    position: 'absolute',
-    top: 0,
-    bottom: 0,
-    left: 0,
-    right: 0
-  },
-  TextField: {
-    marginTop: 20
-  },
-  RightSection: {
-    padding: 20,
-    backgroundImage: `url(${BackgroundImage2})`,
-    backgroundSize: 'cover'
-  },
-  Logo: {
-    height: 50,
-    width: 'auto',
-    display: 'block',
-    marginLeft: 'auto',
-    marginRight: 'auto'
-  },
-  ButtonLogin: {
-    marginTop: 20,
-    marginLeft: 'auto',
-    marginRight: 'auto',
-    display: 'block',
-    backgroundColor: '#94fc13'
-  },
-  card: {
-    minWidth: 275,
-  },
-  bullet: {
-    display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)',
-  },
-  title: {
-    fontSize: 14,
-  },
-  pos: {
-    marginBottom: 12,
-  },
-  cardStyle: {
-    // height : 300,
-    // width : 500,
-    // background: '#94fc13',
-    // marginTop : 100,
-    // marginLeft : 380
-    position: 'relative',
-    width: '400px',
-    height: '350px',
-    top: 'calc(50% - 175px)',
-    left: 'calc(50% - 190px)',
-    borderRadius: '2px',
-    overflow: 'hidden',
-    boxShadow: 'inset 0 0 0 200px rgba(255,255,255,0.05)',
-    opacity: 0.9
-  },
-  Judul: {
-    marginLeft: 70
-  }
-}))
 
 const LoginRoute = () => {
   const classes = useStyle();
--- src/ui/routes/login/style.ts
+++ src/ui/routes/login/style.ts
@@ -13,10 +13,6 @@ export const useStyle = makeStyles(theme => ({
   TextField: {
     marginTop: 20
   },
-  LeftSection: {
-    backgroundImage: `url(${BackgroundImage})`,
-    backgroundSize: 'cover'
-  },
   RightSection: {
     padding: 20,
     backgroundImage: `url(${BackgroundImage2})`,
@@ -33,6 +29,40 @@ export const useStyle = makeStyles(theme => ({
     marginTop: 20,
     marginLeft: 'auto',
     marginRight: 'auto',
-    display: 'block'
+    display: 'block',
+    backgroundColor: '#94fc13'
+  },
+  card: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+  cardStyle: {
+    // height : 300,
+    // width : 500,
+    // background: '#94fc13',
+    // marginTop : 100,
+    // marginLeft : 380
+    position: 'relative',
+    width: '400px',
+    height: '350px',
+    top: 'calc(50% - 175px)',
+    left: 'calc(50% - 190px)',
+    borderRadius: '2px',
+    overflow: 'hidden',
+    boxShadow: 'inset 0 0 0 200px rgba(255,255,255,0.05)',
+    opacity: 0.9
+  },
+  Judul: {
+    marginLeft: 70
   }
 }))
